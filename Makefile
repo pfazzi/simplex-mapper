40331efe4231a22195129e59b723d5fca256f70a
pfazzi <patrick91@live.it>
2022-01-05 00:50:58 +0100

    Refactoring for readability

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 cs-fix:
 	tools/php-cs-fixer/vendor/bin/php-cs-fixer fix --config .php-cs-fixer.dist.php -v
+
+psalm:
+	./vendor/bin/psalm
--- src/Mapper.php
+++ src/Mapper.php
@@ -4,90 +4,37 @@ declare(strict_types=1);
 
 namespace Pfazzi\SimplexMapper;
 
-/** @psalm-suppress MixedAssignment */
+/**
+ * @psalm-suppress MixedAssignment
+ */
 class Mapper
 {
     /**
      * @psalm-param class-string $target
+     *
+     * @throws \ReflectionException
      */
     public function map(array|object $source, string $target, ?NameConverter $nameConverter = null): object
     {
-        $ref = new \ReflectionClass($target);
-        $instance = $ref->newInstanceWithoutConstructor();
+        $targetClassRef = new \ReflectionClass($target);
+        $instance = $targetClassRef->newInstanceWithoutConstructor();
 
         $assign = fn (string $prop, mixed $val): mixed => $this->$prop = $val;
 
-        foreach ($ref->getDefaultProperties() as $property => $defaultValue) {
-            $assign->call($instance, $property, $defaultValue);
-        }
+        $this->assignDefaultValuesToTargetObject($targetClassRef, $assign, $instance);
 
-        if ($constructorRef = $ref->getConstructor()) {
-            foreach ($constructorRef->getParameters() as $parameter) {
-                if ($parameter->isPromoted() && $parameter->isDefaultValueAvailable()) {
-                    $assign->call($instance, $parameter->getName(), $parameter->getDefaultValue());
-                }
-            }
-        }
+        $sourceArray = $this->mapSourceToArray($source);
 
-        if (\is_object($source) && !$source instanceof \stdClass) {
-            $sourceArray = [];
-            $sourceRef = new \ReflectionClass($source);
-            foreach ($sourceRef->getProperties() as $reflectionProperty) {
-                $name = $reflectionProperty->getName();
-                if (PHP_MAJOR_VERSION < 8 || PHP_MINOR_VERSION === 0) {
-                    $reflectionProperty->setAccessible(true);
-                }
-                $value = $reflectionProperty->getValue($source);
-                $sourceArray[$name] = $value;
-            }
-        } else {
-            $sourceArray = $source;
-        }
+        foreach ($sourceArray as $propertyName => $value) {
+            $this->assertPropertyIsString($propertyName);
 
-        foreach ($sourceArray as $property => $value) {
-            if (!is_string($property)) {
-                throw new \RuntimeException('Unable to map int key');
-            }
             if ($nameConverter) {
-                $property = $nameConverter->convert($property);
-            }
-            $propertyType = $ref->getProperty($property)->getType();
-            if (null !== $value && null !== $propertyType) {
-                $valueType = \gettype($value);
-                if ('object' === $valueType) {
-                    $valueType = $value::class;
-                }
-
-                if ($propertyType instanceof \ReflectionUnionType) {
-                    $propertyTypeNames = array_map(
-                        fn (\ReflectionNamedType $t): string => $t->getName(),
-                        $propertyType->getTypes()
-                    );
-                    if (!in_array($valueType, $propertyTypeNames)) {
-                        foreach ($propertyTypeNames as $propertyTypeName) {
-                            try {
-                                $value = self::cast($value, $propertyTypeNames[0]);
-                            } catch (\Exception $exception) {
-                            }
-                        }
-                        $propertyTypeNames = implode('|', $propertyTypeNames);
-                        throw new \RuntimeException("Unable to cast '$valueType' to '$propertyTypeNames'}");
-                    }
-                } elseif ($propertyType instanceof \ReflectionNamedType) {
-                    $propertyTypeName = $propertyType->getName();
-                    if ($propertyTypeName !== $valueType) {
-                        try {
-                            $value = self::cast($value, $propertyTypeName);
-                        } catch (\Exception $exception) {
-                            throw new \RuntimeException("Unable to cast '$valueType' to '$propertyType': {$exception->getMessage()}");
-                        }
-                    }
-                } else {
-                    throw new \RuntimeException('Unable to deserialize intersection types');
-                }
+                $propertyName = $nameConverter->convert($propertyName);
             }
 
-            $assign->call($instance, $property, $value);
+            $value = $this->convertValueForTarget($targetClassRef, $propertyName, $value);
+
+            $assign->call($instance, $propertyName, $value);
         }
 
         return $instance;
@@ -104,5 +51,123 @@ class Mapper
         }
 
         throw new \RuntimeException("Unhandled type '$type'");
+    }
+
+    private function assignDefaultValuesToTargetObject(\ReflectionClass $targetClassRef, \Closure $assigner, object $instance): void
+    {
+        foreach ($targetClassRef->getDefaultProperties() as $property => $defaultValue) {
+            $assigner->call($instance, $property, $defaultValue);
+        }
+
+        if ($constructorRef = $targetClassRef->getConstructor()) {
+            foreach ($constructorRef->getParameters() as $parameter) {
+                if ($parameter->isPromoted() && $parameter->isDefaultValueAvailable()) {
+                    $assigner->call($instance, $parameter->getName(), $parameter->getDefaultValue());
+                }
+            }
+        }
+    }
+
+    private function extractDataFrom(object $source): array
+    {
+        $sourceArray = [];
+
+        $sourceRef = new \ReflectionClass($source);
+        foreach ($sourceRef->getProperties() as $reflectionProperty) {
+            $name = $reflectionProperty->getName();
+            if (PHP_MAJOR_VERSION < 8 || PHP_MINOR_VERSION === 0) {
+                $reflectionProperty->setAccessible(true);
+            }
+            $value = $reflectionProperty->getValue($source);
+            $sourceArray[$name] = $value;
+        }
+
+        return $sourceArray;
+    }
+
+    /**
+     * @psalm-assert string $property
+     */
+    private function assertPropertyIsString(mixed $property): void
+    {
+        if (!is_string($property)) {
+            throw new \RuntimeException('Unable to map int key');
+        }
+    }
+
+    private function mapSourceToArray(object|array $source): array|object
+    {
+        if (\is_object($source) && !$source instanceof \stdClass) {
+            $sourceArray = $this->extractDataFrom($source);
+        } else {
+            $sourceArray = $source;
+        }
+
+        return $sourceArray;
+    }
+
+    private function convertValueForTarget(\ReflectionClass $classRef, string $propertyName, mixed $value): mixed
+    {
+        $property = $classRef->getProperty($propertyName);
+        $propertyType = $property->getType();
+
+        if (null === $propertyType) {
+            return $value;
+        }
+
+        if (null === $value && !$propertyType->allowsNull()) {
+            throw new \RuntimeException('Unable to set null value to property '.$property->getName());
+        }
+
+        $valueType = \gettype($value);
+        if ('object' === $valueType) {
+            $valueType = $value::class;
+        }
+
+        if ($propertyType instanceof \ReflectionUnionType) {
+            return $this->convertValueForUnionType($propertyType, $valueType, $value);
+        }
+
+        if ($propertyType instanceof \ReflectionNamedType) {
+            return $this->convertValueForNamedType($propertyType, $valueType, $value);
+        }
+
+        throw new \RuntimeException('Unable to deserialize intersection types');
+    }
+
+    private function convertValueForUnionType(\ReflectionUnionType $propertyType, string $valueType, mixed $value): mixed
+    {
+        $propertyTypeNames = array_map(
+            fn (\ReflectionNamedType $t): string => $t->getName(),
+            $propertyType->getTypes()
+        );
+
+        if (in_array($valueType, $propertyTypeNames)) {
+            return $value;
+        }
+
+        foreach ($propertyTypeNames as $propertyTypeName) {
+            try {
+                return self::cast($value, $propertyTypeName);
+            } catch (\Exception $exception) {
+            }
+        }
+
+        $propertyTypeNames = implode('|', $propertyTypeNames);
+        throw new \RuntimeException("Unable to cast '$valueType' to '$propertyTypeNames'}");
+    }
+
+    private function convertValueForNamedType(\ReflectionNamedType $propertyType, string $valueType, mixed $value): mixed
+    {
+        $propertyTypeName = $propertyType->getName();
+        if ($propertyTypeName === $valueType) {
+            return $value;
+        }
+
+        try {
+            return self::cast($value, $propertyTypeName);
+        } catch (\Exception $exception) {
+            throw new \RuntimeException("Unable to cast '$valueType' to '$propertyType': {$exception->getMessage()}");
+        }
     }
 }
